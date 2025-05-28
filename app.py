74b07e6e2ff49a47c5f8c41367a87df33bda3383
adrianor85 <adrianor85@gmail.com>
2025-05-28 11:00:40 -0300

    The app file was updated

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from config import app_config, app_active, Config
 config = app_config['testing']
 
 def create_app(config_name):
-  app = Flask(__name__, template_folder=templates)
+  app = Flask(__name__, template_folder='templates')
 
   app.secret_key = config.SECRETE
   app.config.from_object(app_config[config_name])
