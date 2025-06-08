477bac144b77920ac69f8a8027731855619cfa19
AdrianoR85 <adrianorosa085@gmail.com>
2025-06-08 09:16:39 -0300

    Restarted the project

--- run.py
+++ run.py
@@ -1,8 +1,1 @@
-from app import create_app
-from config import app_config, app_active
 
-app = create_app(app_active)
-
-if __name__ == "__main__":
-    config = app_config[app_active]
-    app.run(host=config.IP_HOST, port=config.PORT_HOST)
