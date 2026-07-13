1f3e4ca1a2a135f91f2df7171cb47d362945dcec
adrianor85 <adriano.study31@gmail.com>
2026-07-13 14:48:39 -0300

    verify and generate token function were created

--- app/controller/User.py
+++ app/controller/User.py
@@ -2,6 +2,11 @@ from unittest import result
 from app.model import User
 from app.extensions import logger
 
+import jwt
+from datetime import datetime, timedelta
+from config import Config
+
+
 class UserController:
     @staticmethod
     def login(email, password):
@@ -24,3 +29,47 @@ class UserController:
             result['status'] = 500
             result['message'] = str(e)
         return result
+    
+    @staticmethod
+    def verify_auth_token(token):
+        try:
+            payload = jwt.decode(
+                token,
+                Config.SECRET_KEY,
+                algorithms=["HS256"]
+            )
+
+            return {
+                "status": 200,
+                "message": "Token válido",
+                "data": payload
+            }
+
+        except jwt.ExpiredSignatureError:
+            return {
+                "status": 401,
+                "message": "Token expirado"
+            }
+
+        except jwt.InvalidTokenError:
+            return {
+                "status": 401,
+                "message": "Token inválido"
+            }
+
+    @staticmethod
+    def generate_auth_token(data, exp=30):
+        
+        expiration = datetime.now(datetime.timezone.utc) + timedelta(minutes=exp)
+
+        payload = {
+            "id": data["id"],
+            "username": data["username"],
+            "exp": expiration
+        }
+
+        return jwt.encode(
+            payload,
+            Config.SECRET_KEY,
+            algorithm="HS256"
+        )
